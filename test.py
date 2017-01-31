cd6c5425137ea5b2fdf10be47d7772789460dd9a
Taylor <bizzytay7@yahoo.com>
2017-01-31 12:10:20 -0700

    second commit

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 # TEAM LEADER:
 # implement this function so that it returns copy of string_arg reversed
 def reverseWord(string_arg):
-	pass
+	return string_arg[::-1]
 
 # TEAM MEMBER:
 # implement this function so that it returns the frequency of query in string_arg
